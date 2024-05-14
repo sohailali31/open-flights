e0bc2cb88fbc9bcd36e8b4ceaeb8616b8b18bf43
sohailali31 <158456702+sohailali31@users.noreply.github.com>
2024-05-15 01:46:18 +0530

    Delete argocdserver.sh

--- argocdserver.sh
+++
@@ -1,8 +0,0 @@
-kubectl create namespace argocd
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-sudo apt update
-sudo apt install jq -y
-export ARGOCD_SERVER=$(kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname')
-echo $ARGOCD_SERVER
-kubectl get svc argocd-server -n argocd -o json | jq --raw-output .status.loadBalancer.ingress[0].hostname
-
